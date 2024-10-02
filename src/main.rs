5bde3c81ce5e7ffea059754927a3541a6e3bb05b
Kayky Belleboni Casagrande <k.belleboni@edu.pucrs.br>
2024-10-01 23:10:06 -0300

    feat: read from file the ports

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
+use std::{
+    fs::File,
+    io::{BufRead, BufReader},
+    path::PathBuf,
+};
+
+use clap::Parser;
 use futures::future::join_all;
 use tokio::{
     net::{TcpListener, TcpStream},
@@ -5,31 +12,27 @@ use tokio::{
 };
 
 const LOCALHOST: &str = "127.0.0.1";
+const DELIMITER: &str = "->";
 
 struct Proxy {
     from: u16,
     to: u16,
-    listener: TcpListener,
 }
 
 impl Proxy {
-    async fn new(from: u16, to: u16) -> tokio::io::Result<Self> {
-        Ok(Self {
-            from,
-            to,
-            listener: TcpListener::bind((LOCALHOST, from)).await?,
-        })
-    }
-
-    async fn accept(&mut self) -> tokio::io::Result<(TcpStream, std::net::SocketAddr)> {
-        self.listener.accept().await
+    fn new(from: u16, to: u16) -> Self {
+        Self { from, to }
     }
 }
 
 async fn redirect(mut socket: TcpStream, from: u16, to: u16) -> tokio::io::Result<()> {
     let instant = Instant::now();
 
-    let mut client = TcpStream::connect((LOCALHOST, to)).await?;
+    let mut client = TcpStream::connect((LOCALHOST, to)).await.map_err(|err| {
+        eprintln!(":{} is off.", to);
+        err
+    })?;
+
     let _ = tokio::io::copy_bidirectional(&mut client, &mut socket).await;
 
     let elapsed = instant.elapsed();
@@ -39,22 +42,42 @@ async fn redirect(mut socket: TcpStream, from: u16, to: u16) -> tokio::io::Resul
     Ok(())
 }
 
-async fn accept_loop(mut proxy: Proxy) {
+async fn accept_loop(proxy: Proxy) -> tokio::io::Result<()> {
+    let listener = TcpListener::bind((LOCALHOST, proxy.from)).await?;
+
     loop {
-        if let Ok((socket, _)) = proxy.accept().await {
+        if let Ok((socket, _)) = listener.accept().await {
             tokio::spawn(redirect(socket, proxy.from, proxy.to));
         }
     }
+}
+
+#[derive(Parser)]
+#[command(version, about, long_about = None)]
+struct Args {
+    #[arg(short, long)]
+    file: Option<PathBuf>,
 }
 
 #[tokio::main]
 async fn main() -> tokio::io::Result<()> {
     println!("Stating proxy...");
 
-    let proxis = join_all(vec![Proxy::new(8080, 3000)])
-        .await
-        .into_iter()
-        .flatten()
+    let args = Args::parse();
+
+    let file_name = args.file.unwrap_or(PathBuf::from("rngix.conf"));
+
+    let file = File::open(file_name)?;
+
+    let proxis = BufReader::new(file)
+        .lines()
+        .map_while(Result::ok)
+        .flat_map(|line| {
+            let (from, to) = line.split_once(DELIMITER)?;
+            let from = from.trim().parse().ok()?;
+            let to = to.trim().parse().ok()?;
+            Some(Proxy::new(from, to))
+        })
         .map(|proxy| tokio::spawn(accept_loop(proxy)));
 
     let _ = join_all(proxis).await;
